fc1c1ee7216dd423a804581e2fc5db45097820c1
Abhijith S <abh78i00@gmail.com>
2026-03-11 17:45:00 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from backend.scanner import run_scan
 from modules.recon.subdomain_scanner import find_subdomains
 from modules.recon.wayback_urls import get_wayback_urls
+from modules.recon.otx_urls import get_otx_urls
 
 app = FastAPI()
 
@@ -20,3 +21,7 @@ def subdomains(domain: str):
 @app.get("/wayback")
 def wayback(domain: str):
     return get_wayback_urls(domain)
+
+@app.get("/otx")
+def otx(domain: str):
+    return get_otx_urls(domain)
