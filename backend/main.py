573adf631875491ad030f80b2be339f4e3871510
Abhijith S <abh78i00@gmail.com>
2026-03-11 16:28:50 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from backend.scanner import run_scan
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "AION-X API running"}
+
+@app.get("/scan")
+def scan(target: str):
+
+    result = run_scan(target)
+
+    return result
