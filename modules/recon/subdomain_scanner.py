e4f998a74ed2c2ccade8dd69ae6637dac420a1a7
Abhijith S <abh78i00@gmail.com>
2026-03-11 17:08:34 +0530

    Update subdomain_scanner.py

--- modules/recon/subdomain_scanner.py
+++ modules/recon/subdomain_scanner.py
@@ -5,7 +5,7 @@ def find_subdomains(domain):
 
     try:
         url = f"https://crt.sh/?q=%25.{domain}&output=json"
-        response = requests.get(url, timeout=10)
+        response = requests.get(url, timeout=30)
 
         if response.status_code == 200:
             data = response.json()
