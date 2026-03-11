56345001e572b2f126e0a7a318fa4acc42441181
Abhijith S <abh78i00@gmail.com>
2026-03-11 16:28:26 +0530

    Update scanner.py

--- backend/scanner.py
+++ backend/scanner.py
@@ -1,1 +1,13 @@
+from modules.network.portscan import scan_ports
 
+def run_scan(target):
+
+    ports = scan_ports(target)
+
+    result = {
+        "target": target,
+        "open_ports": ports,
+        "total_open_ports": len(ports)
+    }
+
+    return result
