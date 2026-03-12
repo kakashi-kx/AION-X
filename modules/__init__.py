cb4339d0514f5c46f7bda078d14006f3e36dd7b3
Abhijith S <abh78i00@gmail.com>
2026-03-12 15:19:33 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,9 @@
+"""
+AION-X Modules Package
+Contains all security testing modules organized by category
+"""
 
+__version__ = "0.1.0"
+__author__ = "Kakashi"
+
+# This file makes the 'modules' directory a Python package
