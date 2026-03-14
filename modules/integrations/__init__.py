856f0eccaff9ae2af0a51f85fff8afa0c42fe709
Abhijith S <abh78i00@gmail.com>
2026-03-14 10:04:16 +0530

    Update __init__.py

--- modules/integrations/__init__.py
+++ modules/integrations/__init__.py
@@ -1,1 +1,6 @@
+"""
+Bug Bounty Integration Modules for AION-X
+"""
+from .bugbounty_platforms import AutoSubmitter, HackerOneIntegration, BugcrowdIntegration
 
+__all__ = ['AutoSubmitter', 'HackerOneIntegration', 'BugcrowdIntegration']
